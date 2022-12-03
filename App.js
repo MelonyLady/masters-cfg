adde056d768b6b9b9357eb42d354a63c06e1a9a7
Sydney Humayun <psychdreamer@hotmail.co.uk>
2022-12-03 13:37:13 +0000

    commented out old navigation

--- App.js
+++ App.js
@@ -3,24 +3,25 @@ import HomeScreen from './src/screens/homescreen';
 import AboutUsScreen from './src/screens/AboutUsScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from 'react-navigation';
 
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
 
-export default function App() {
+// export default function App() {
   
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen name="AboutUs" component={AboutUsScreen} />
-      </Stack.Navigator>
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen
+//           name="Home"
+//           component={HomeScreen}
+//           options={{ title: 'Welcome' }}
+//         />
+//         <Stack.Screen name="AboutUs" component={AboutUsScreen} />
+//       </Stack.Navigator>
       
-    </NavigationContainer>
+//     </NavigationContainer>
      
-  );
-}
+//   );
+// }
