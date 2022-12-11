a2c47c4798ef8c7b3864f7a5ec3f4e8580b6de89
Sydney Humayun <psychdreamer@hotmail.co.uk>
2022-12-11 15:06:20 +0000

    commented out the image problem

--- src/screens/loginScreen.js
+++ src/screens/loginScreen.js
@@ -23,7 +23,7 @@ export default function LoginScreen({ navigation }) {
 
 return (
   <View style={styles.container}>
-    <Image source={require('../melonLady.png')} />
+    {/* <Image source={require('..\..\..\..\..\...\assets\melonLady.png')} /> */}
 
     <View style={styles.inputView}> 
     <TextInput
