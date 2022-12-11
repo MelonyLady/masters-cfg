53c1960ff7608f0502e2f510cd1491ce237ec3e2
Sydney Humayun <psychdreamer@hotmail.co.uk>
2022-12-11 14:35:13 +0000

    attempt at authentication

--- src/screens/loginScreen.js
+++ src/screens/loginScreen.js
@@ -1,23 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput, TouchableOpacity, alert } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
+import {useState} from 'react';
 
 export default function LoginScreen({ navigation }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [successCriteria, setSuccesCriteria] = useState('');
+ 
+  
+  const credentialCheck = () => {
+    setSuccesCriteria('');
+    if (!username) {
+      alert('please enter username');
+      return;
+    }
+    if (!password) {
+      alert('please enter paswword')
+    };
+  }
 
 
 
 return (
   <View style={styles.container}>
-    <Image source={require(".../assets/MelonLady.png")} />
-    <Text style={styles.textHeader}>Please Login below:</Text>
-    <Text></Text>
-    
-    <Text style={styles.textSubheader}>Username: </Text>
-    <Text></Text>
-    <Text style={styles.textSubheader}>Password: </Text>
-    <Text></Text>
+    {/* <Image source={require("../assets/MelonLady.png")} /> */}
+
+    <View style={styles.inputView}> 
+    <TextInput
+    style={styles.textInput}
+    placeholder="Username"
+    placeholderTextColor="#003f5c"
+    onChangeText={(username) => setUsername(username)}
+  />
+    </View>
+    <View style={styles.inputView}>
+    <TextInput
+      style={styles.textInput}
+      placeholder="Password."
+      placeholderTextColor="#003f5c"
+      secureTextEntry={true}
+      onChangeText={(password) => setPassword(password)}
+    />
+      </View>
+    <TouchableOpacity>
+      <Text style={styles.forgot_button}>Forgot Password?</Text>
+      </TouchableOpacity>
+    <TouchableOpacity style={styles.loginBtn}>
+      <Text style={styles.loginText}
+      onPRess={credentialCheck}>LOGIN</Text>
+      </TouchableOpacity>
+
     <StatusBar style="auto" />
   </View>
+  
+   
 );
 }
   
@@ -46,5 +83,37 @@ return (
       color: '#be84a3',
       fontStyle: 'normal',
       fontSize: 19,
-        }
+        },
+    image :{
+      marginBottom: 40
+    
+    },
+    inputView: {
+      backgroundColor: "#A991C8",
+      borderRadius: 30,
+      width: "70%",
+      height: 45,
+      marginBottom: 20,
+      alignItems: "center",
+    },
+    textInput: {
+      height: 50,
+      flex: 1,
+      padding: 10,
+      marginLeft: 20,
+    },
+    forgot_button: {
+      height: 30,
+      marginBottom: 30,
+      color: "#be84a3"
+    },
+    loginBtn:{
+      width:"80%",
+      borderRadius:25,
+      height:50,
+      alignItems:"center",
+      justifyContent:"center",
+      marginTop:40,
+      backgroundColor:"#be84a3",
+    }
   });
